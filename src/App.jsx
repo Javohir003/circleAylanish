4c4fbcb684dba21b46c86d4578a7e8e875c3c8d6
Javohir <yusupovjavoxir11@gmail.com>
2024-05-04 22:41:33 +0500

    bu iqtidordagi tanga animatsiyasi

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <div className='box'>
+          <div className='liner'></div>
+          <h2>02</h2>
+        </div>
+    </>
+  )
+}
+
+export default App
